9052137ec24eaf3988f3f86ec02662dec52407d9
Muskan Gupta <muskan.gupta@arya.ag>
2025-10-06 02:12:36 +0530

    LengthOfLongestSubstring using 2 pointer<>sliding window

---
+++ LengthOfLongestSubstring.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class LengthOfLongestSubstring {
+     public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLen = 0;
+        for(int right = 0;right<s.length();right++) {
+            Character ch = s.charAt(right);
+            while(set.contains(ch)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(ch);
+            maxLen = Math.max(maxLen, right - left+1);
+        }
+        return maxLen;
+    }
+}
